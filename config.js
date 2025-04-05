771f40eba5f722e99db7cdca1901dadbacf3cf60
William Holley <william2sober@gmail.com>
2025-04-05 14:26:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  clientId: '1355936471932211210',
+  clientId: '',
   tickets: {
-    ownerID: '1342676516353343513',
-    category: '1358146035150491720', // replace with actual category ID
-    staffRole: '1358089585644802168',
-    whitelistRole: '1358089585644802168',
-    logChannel: '1358087183390474411',
-    sendEmbedChannel: '1358087169515978792', // where the ticket embed auto-sends
+    ownerID: '',
+    category: '', 
+    staffRole: '',
+    whitelistRole: '',
+    logChannel: '',
+    sendEmbedChannel: '',
   },
 };
